002ebfa1cd63c4a42202a4ef959afdf2b5834c85
marcosrdef <marcosrdef@gmail.com>
2018-12-01 20:48:12 -0200

    ajustes

--- RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy2/Model/Person.cs
+++ RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy2/Model/Person.cs
@@ -11,6 +11,6 @@ namespace RestWithASPNETUdemy2.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
-        public string Genering { get; set; }
+        public string Gender { get; set; }
     }
 }
