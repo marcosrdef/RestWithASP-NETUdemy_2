7ee3f7a3e699b4ad741b8aff90b94939a86d7c18
marcosrdef <marcosrdef@gmail.com>
2018-12-01 20:45:09 -0200

    Implementing all verbs

--- RestWithASPNETUdemy 01- Calculator/RestWithASPNETUdemy2/Controllers/ValuesController.cs
+++ RestWithASPNETUdemy 01- Calculator/RestWithASPNETUdemy2/Controllers/ValuesController.cs
@@ -10,13 +10,82 @@ namespace RestWithASPNETUdemy2.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
-        // GET api/values/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        // GET api/sum/5/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public ActionResult<string> Sum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Inválid Input");
+        }
+
+        // GET api/subtraction/5/5
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Inválid Input");
+        }
+
+        // GET api/division/5/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if (ConvertToDecimal(secondNumber) == 0)
+                {
+                    return BadRequest("Não é possível fazer divisão por 0");
+                }
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Inválid Input");
+        }
+
+        // GET api/multiplication/5/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Inválid Input");
+        }
+
+        // GET api/mean/5/5
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Inválid Input");
+        }
+
+        // GET api/square-root/4
+        [HttpGet("square-root/{firstNumber}")]
+        public ActionResult<string> SquareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var sum = Math.Sqrt((double)ConvertToDecimal(firstNumber));
                 return Ok(sum.ToString());
             }
 
