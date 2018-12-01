3ecc6fb89442aa20cebb811b8c751a558e520edd
marcosrdef <marcosrdef@gmail.com>
2018-12-01 21:52:46 -0200

    adicionando os verbos

---
+++ RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy2/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy2.Model;
+
+namespace RestWithASPNETUdemy2.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i =0; i<8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + i,
+                LastName = "Person Name " + i,
+                Address = "Some Address " + i,
+                Gender = "Gnder Person " + i
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Marcos Roberto",
+                LastName = "Freitas Junior",
+                Address = "São Paulo - SP - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
--- RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy2/Startup.cs
+++ RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy2/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestWithASPNETUdemy2.Services;
+using RestWithASPNETUdemy2.Services.Implementations;
 
 namespace RestWithASPNETUdemy2
 {
@@ -26,6 +28,7 @@ namespace RestWithASPNETUdemy2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
